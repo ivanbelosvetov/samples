848f6cf706be68bb0df110795bbe1d4a0ab602b7
ivanbelosvetov <79517671+ivanbelosvetov@users.noreply.github.com>
2021-03-07 22:10:29 +0200

    fix: fix this file popups.js

--- js/popups.js
+++ js/popups.js
@@ -4,7 +4,7 @@ let popup_button = document.getElementById('popup__button');
 let popup_button2 = document.getElementById('popup__button2');
 let images = document.getElementById('img__11');
 let imgNumber = 0;
-let imageSources = ["https://github.com/ivanbelosvetov/samples/gh-pages/img/Fishdom_1.jpg", "https://github.com/ivanbelosvetov/samples/gh-pages/img/Fishdom.jpg"]
+//let imageSources = ["https://github.com/ivanbelosvetov/samples/gh-pages/img/Fishdom_1.jpg", "https://github.com/ivanbelosvetov/samples/gh-pages/img/Fishdom.jpg"]
 button.onclick = function() {
 	textarea.classList.add('edit');
 
@@ -12,40 +12,40 @@ button.onclick = function() {
 
 popup_button.onclick = function () {
 	images.src = imageSources[imgNumber];
-	if (imgNumber < 1) {
+	/*if (imgNumber < 1) {
 		imgNumber = imgNumber + 1;
 	} else if (imgNumber > 0) {
 		imgNumber = imgNumber - 1;
-	}
+	}*/
 	
 	
 }
 
 popup_button2.onclick = function () {
 	images.src=imageSources[imgNumber];
-	if (imgNumber < 1) {
+	/*if (imgNumber < 1) {
 		imgNumber = imgNumber + 1;
 	} else if (imgNumber > 0) {
 		imgNumber = imgNumber - 1;
-	}
+	}*/
 }
 
 document.body.onkeydown = (k) => {
 	if(k.key=="ArrowLeft") {
 		images.src = imageSources[imgNumber];
-	if (imgNumber < 1) {
+	/*if (imgNumber < 1) {
 		imgNumber = imgNumber + 1;
 	} else if (imgNumber > 0) {
 		imgNumber = imgNumber - 1;
 	}
-	}
+	}*/
 	if(k.key=="ArrowRight") {
-		images.src = imageSources[imgNumber];
-	if (imgNumber < 1) {
+		images.src = samples/img/Fishdom.jpg;
+	/*if (imgNumber < 1) {
 		imgNumber = imgNumber + 1;
 	} else if (imgNumber > 0) {
 		imgNumber = imgNumber - 1;
-	}
+	}*/
   }	 
 
 }
